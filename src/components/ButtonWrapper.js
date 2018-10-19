c92c42dbcb9d5e287c7f1d3bde4911634e8d4e3c
Mihajlo Simevski <s.mihajlo992@gmail.com>
2018-10-19 02:53:58 +0200

    Update ButtonWrapper.js

--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -24,25 +24,25 @@ return(
         <Button content="/" color="red" />
         <Button content="x" color="red" />
     </Button.Group>
-    <Button.Group widths="4" disabled>
+    <Button.Group widths="4" >
         <Button content="7" />
         <Button content="8" />
         <Button content="9" />
         <Button content="-" />
     </Button.Group>
-    <Button.Group widths="4" disabled>
+    <Button.Group widths="4" >
         <Button content="4" />
         <Button content="5" />
         <Button content="6" />
         <Button content="+" />
     </Button.Group>
-    <Button.Group widths="4" disabled>
+    <Button.Group widths="4" >
         <Button content="1" />
         <Button content="2" />
         <Button content="3" />
         <Button content="=" />
     </Button.Group>
-    <Button.Group widths="2" disabled>
+    <Button.Group widths="2" >
         <Button content="0" />
         <Button content="." color="blue" />
     </Button.Group>
